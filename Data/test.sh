5eee26ef8454bbc72a63a6bfe456e47f80515ac2
740291272 <740291272@qq.com>
2018-06-22 10:32:03 +0800

    Chage & clea

--- Data/test.sh
+++ Data/test.sh
@@ -1,7 +1,2 @@
-./build/sinsyNG -o xtest4.wav Test4.xml
-./build/sinsyNG -o xtest3.wav Test3.xml
-./build/sinsyNG -o xtest2.wav Test2.xml
-./build/sinsyNG -o xtest.wav Test.xml
-sox xtest*.wav test_musicxml.wav
-./build/sinsyNG -u test.sinsy -o test_uscore.wav
+../build/sinsyNG -u test.sinsy -o test_uscore.wav
 
