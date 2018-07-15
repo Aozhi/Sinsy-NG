ac2fe790fbbce28e661e5593daa59bf15972c50a
740291272 <740291272@qq.com>
2018-07-15 13:38:19 +0800

    back to macos

--- Data/test.sh
+++ Data/test.sh
@@ -1,3 +1,2 @@
-# ../build/sinsyNG -o csongdb_f00002_000.wav csongdb_f00002_000.xml
 ../build/sinsyNG -u test.sinsy -o test_uscore.wav
 
---
+++ build/CMakeFiles/sinsy.dir/DependInfo.cmake
@@ -0,0 +1,74 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ghostgloomy/vs/sinsy/lib/Sinsy.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/Sinsy.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/converter/ConfGroup.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/converter/ConfGroup.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/converter/ConfManager.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/converter/ConfManager.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/converter/Converter.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/converter/Converter.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/converter/PhonemeInfo.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/converter/PhonemeInfo.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/converter/UnknownConf.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/converter/UnknownConf.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/converter/util_converter.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/converter/util_converter.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/hts_engine_API/HtsEngine.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/hts_engine_API/HtsEngine.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/hts_engine_API/SynthConditionImpl.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/hts_engine_API/SynthConditionImpl.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/japanese/JConf.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/japanese/JConf.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/label/LabelData.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/label/LabelData.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/label/LabelMaker.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/label/LabelMaker.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/label/LabelMeasure.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/label/LabelMeasure.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/label/LabelPosition.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/label/LabelPosition.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/label/LabelStream.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/label/LabelStream.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/label/LabelStrings.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/label/LabelStrings.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/label/NoteGroup.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/label/NoteGroup.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/label/NoteLabeler.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/label/NoteLabeler.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/label/PhonemeLabeler.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/label/PhonemeLabeler.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/label/SyllableLabeler.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/label/SyllableLabeler.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/score/Beat.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/score/Beat.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/score/Dynamics.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/score/Dynamics.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/score/Key.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/score/Key.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/score/Mode.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/score/Mode.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/score/Note.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/score/Note.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/score/Pitch.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/score/Pitch.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/score/ScorePosition.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/score/ScorePosition.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/score/Slur.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/score/Slur.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/score/Syllabic.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/score/Syllabic.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/score/util_score.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/score/util_score.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/shm_ringbuffer/ringbuffer.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/shm_ringbuffer/ringbuffer.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/temporary/ScoreDoctor.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/temporary/ScoreDoctor.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/temporary/TempScore.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/temporary/TempScore.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/util/Configurations.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/util/Configurations.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/util/DiphthongConverter.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/util/DiphthongConverter.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/util/IStringable.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/util/IStringable.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/util/InputFile.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/util/InputFile.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/util/MacronTable.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/util/MacronTable.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/util/MultibyteCharRange.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/util/MultibyteCharRange.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/util/OutputFile.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/util/OutputFile.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/util/PhonemeTable.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/util/PhonemeTable.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/util/StringTokenizer.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/util/StringTokenizer.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/util/util_string.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/util/util_string.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/xml/XmlData.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/xml/XmlData.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/xml/XmlParser.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/xml/XmlParser.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/xml/XmlReader.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/xml/XmlReader.o"
+  "/Users/ghostgloomy/vs/sinsy/lib/xml/XmlWriter.cpp" "/Users/ghostgloomy/vs/sinsy/build/CMakeFiles/sinsy.dir/lib/xml/XmlWriter.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/util"
+  "../lib/score"
+  "../lib/japanese"
+  "../lib/converter"
+  "../lib/temporary"
+  "../lib/xml"
+  "../lib/shm_ringbuffer"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
